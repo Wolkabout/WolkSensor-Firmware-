b6630bfa955ac3bf02140b18cacb893efa7b6c61
srdjastankovic <sstankovic@execom.eu>
2018-03-24 20:08:24 +0100

    update banner

--- wolksensor/tests/functional/initialisation/initialisation.py
+++ wolksensor/tests/functional/initialisation/initialisation.py
@@ -11,4 +11,8 @@ def initialisation():
     logging_device.set_level(logging_device._info_)
     logging_device.info("")
 
-    print constants.LOGO + "\n\r\t\t   Functional Testing\n\r==========================================================="
+    #print constants.LOGO + "\n\r\t\t   Functional Testing\n\r==========================================================="
+    
+    print '\n\r===========================================================\n\r' + constants.LOGO + '\n\r\t\t   ' + constants.NAME \
+    + "\n\r\t\t\tV" + str(constants.BLESSERVERSIONMAJOR) + '.' + str(constants.BLESSERVERSIONMINOR) + '.' + str(constants.BLESSERVERSIONPATCH) \
+    + '\n\r==========================================================='
