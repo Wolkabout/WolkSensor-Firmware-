6753784a694320d51c23de0a22aa8a44bedd6306
srdjastankovic <sstankovic@execom.eu>
2018-03-28 16:44:46 +0200

    increase verison

--- wolksensor/wolksensor/WolkSensor/platform_specific.h
+++ wolksensor/wolksensor/WolkSensor/platform_specific.h
@@ -15,7 +15,7 @@
 
 #define FW_VERSION_MAJOR 4 // number 0 -99
 #define FW_VERSION_MINOR 3 // number 0 -99
-#define FW_VERSION_PATCH 4 // number 0 -99
+#define FW_VERSION_PATCH 5 // number 0 -99
 
 #define SYNCHRONIZED_BLOCK_START register8_t saved_sreg = SREG; cli();
 #define SYNCHRONIZED_BLOCK_END SREG = saved_sreg;
@@ -29,7 +29,7 @@
 
 #define MAX_BUFFER_SIZE 768
 
-#define NUMBER_OF_ACTUATORS 0 
+#define NUMBER_OF_ACTUATORS 0
 
 #define NUMBER_OF_SENSORS 4
 
